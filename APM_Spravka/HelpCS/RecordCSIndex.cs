be276fdd3c52cbc687d8ba79b1820f7f6f1ba1ce
Vladislav Gaidukevich <vladislav.gajdukevich@gmail.com>
2019-04-19 04:04:10 +0300

    Доделан макет

---
+++ APM_Spravka/HelpCS/RecordCSIndex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APM_Spravka
+{
+    public class RecordCSIndex
+    {
+        private string mounth;
+        private decimal doxod;
+        private Int32 index;
+
+        public RecordCSIndex(string mounth, decimal doxod, int index)
+        {
+            this.mounth = mounth;
+            this.doxod = doxod;
+            this.index = index;
+        }
+
+        public RecordCSIndex()
+        {
+        }
+
+        public string Mounth
+        {
+            get => mounth;
+            set => mounth = value;
+        }
+
+        public decimal Doxod
+        {
+            get => doxod;
+            set => doxod = value;
+        }
+
+        public int Index
+        {
+            get => index;
+            set => index = value;
+        }
+    }
+
+
+}
--- APM_Spravka/Record.xaml.cs
+++ APM_Spravka/Record.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static APM_Spravka.RecordCS;
+using static APM_Spravka.RecordCSIndex;
 
 namespace APM_Spravka
 {
