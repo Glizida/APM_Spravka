83178b2610b37ec3f8486a385b19aebd034b0fd6
Vladislav Gaidukevich <vladislav.gajdukevich@gmail.com>
2019-05-18 00:36:39 +0300

    Добавленна авторизация

---
+++ APM_Spravka/BD Table/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APM_Spravka.BD_Table
+{
+    public class User
+    {
+        private int idUser;
+        private string loginUser;
+        private string passwordUser;
+        private int levelAccess;
+
+        public User()
+        {
+        }
+
+        public User(int idUser, string loginUser, string passwordUser, int levelAccess)
+        {
+            this.idUser = idUser;
+            this.loginUser = loginUser;
+            this.passwordUser = passwordUser;
+            this.levelAccess = levelAccess;
+        }
+        public int IdUser
+        {
+            get => idUser;
+            set => idUser = value;
+        }
+
+        public string LoginUser
+        {
+            get => loginUser;
+            set => loginUser = value;
+        }
+
+        public string PasswordUser
+        {
+            get => passwordUser;
+            set => passwordUser = value;
+        }
+
+        public int LevelAccess
+        {
+            get => levelAccess;
+            set => levelAccess = value;
+        }
+    }
+}
---
+++ APM_Spravka/Login.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using APM_Spravka.BD_Table;
+using APM_Spravka_Admin.BD_Table;
+using MySql.Data.MySqlClient;
+
+namespace APM_Spravka
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public string CONNECT = "Database=nzzGtRxVKL;Data Source=remotemysql.com;User Id=nzzGtRxVKL;Password=OCqp3u3YNf";
+        public List<User> users = new List<User>();
+        string CommandText = "";
+
+        MySqlCommand myCommand = new MySqlCommand();
+        MySqlDataReader MyDataReader;
+       
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection myConnection = new MySqlConnection(CONNECT);
+            try
+            {
+                if (LoginTexBox.Text != "" || PasswordTexBox.Text != "")
+                {
+                    CommandText = "SELECT * FROM nzzGtRxVKL.User where LoginUser = \""+LoginTexBox.Text+ "\";";
+                    myCommand = new MySqlCommand(CommandText, myConnection);
+                    myConnection.Open();
+                    MyDataReader = myCommand.ExecuteReader();
+                    while (MyDataReader.Read())
+                    {
+                        users.Add(new User(MyDataReader.GetInt32(0), MyDataReader.GetString(1), MyDataReader.GetString(2), MyDataReader.GetInt32(3)));
+                    }
+                    MyDataReader.Close();
+                    if (users.Count != 0)
+                    {
+                        if (users[0].PasswordUser == PasswordTexBox.Text)
+                        {
+                            switch (users[0].LevelAccess)
+                            {
+                                case 1:
+                                    MainWindow mainWindow = new MainWindow();
+                                    mainWindow.Owner = this;
+                                    mainWindow.Show();
+                                    this.Hide();
+                                    break;
+                                case 2:
+                                    MessageBox.Show("Admin2");
+                                    break;
+                                case 3:
+                                    MessageBox.Show("Admin3");
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Не верный логин или пароль", "Ошибка", MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь с таким логином не найден", "Ошибка", MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста введите логин и пароль");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Closed_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
